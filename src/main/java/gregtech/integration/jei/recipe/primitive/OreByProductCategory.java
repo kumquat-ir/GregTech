fc9eb4e0479bac8e079e260888e9631cca1e181e
kumquat-ir <66188216+kumquat-ir@users.noreply.github.com>
2021-08-12 17:53:41 -0400

    initial layout stuff

--- src/main/java/gregtech/integration/jei/recipe/primitive/OreByProductCategory.java
+++ src/main/java/gregtech/integration/jei/recipe/primitive/OreByProductCategory.java
@@ -1,5 +1,7 @@
 package gregtech.integration.jei.recipe.primitive;
 
+import com.google.common.collect.ImmutableList;
+import gregtech.api.GTValues;
 import gregtech.api.gui.GuiTextures;
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.gui.*;
@@ -7,15 +9,33 @@ import mezz.jei.api.gui.IDrawableAnimated.StartDirection;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.IRecipeWrapper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.ResourceLocation;
 
 import javax.annotation.Nonnull;
 
 public class OreByProductCategory extends PrimitiveRecipeCategory<OreByProduct, OreByProduct> {
 
     protected final IDrawable slot;
-    protected final IDrawable arrowBackground;
-    protected final IDrawableStatic arrowForeground;
-    protected final IDrawableAnimated arrowAnimation;
+    protected final IDrawable arrowsStatic;
+
+    protected final static ImmutableList<Integer> BASIC_PREFIX_LOCATIONS = ImmutableList.of(
+            79, 4,      // ore
+            79, 29,     // crushed
+            92, 54,     // crushedPurified
+            117, 54,    // crushedCentrifuged
+            41, 54,     // dustImpure
+            66, 54,     // dustPure
+            79, 126     // dust
+    );
+    // main locations for byproducts
+    protected final static ImmutableList<Integer> BYPRODUCT_LOCATIONS = ImmutableList.of(
+            4, 126,
+            29, 126,
+            54, 126,
+            105, 126,
+            129, 126,
+            154, 126
+    );
 
     public OreByProductCategory(IGuiHelper guiHelper) {
         super("ore_by_product",
@@ -24,24 +44,30 @@ public class OreByProductCategory extends PrimitiveRecipeCategory<OreByProduct, 
                 guiHelper);
 
         this.slot = guiHelper.drawableBuilder(GuiTextures.SLOT.imageLocation, 0, 0, 18, 18).setTextureSize(18, 18).build();
-        this.arrowBackground = guiHelper.drawableBuilder(GuiTextures.PROGRESS_BAR_ARROW.imageLocation, 0, 0, 20, 20).setTextureSize(20, 40).build();
-        this.arrowForeground = guiHelper.drawableBuilder(GuiTextures.PROGRESS_BAR_ARROW.imageLocation, 0, 20, 20, 20).setTextureSize(20, 40).build();
-        this.arrowAnimation = guiHelper.createAnimatedDrawable(arrowForeground, 30, StartDirection.LEFT, false);
+        this.arrowsStatic = guiHelper.drawableBuilder(new ResourceLocation(GTValues.MODID + ":textures/gui/arrows/byproducts_static.png"),
+                0, 0, 81, 43).setTextureSize(81, 43).build();
     }
 
     @Override
     public void setRecipe(IRecipeLayout recipeLayout, OreByProduct recipeWrapper, @Nonnull IIngredients ingredients) {
         IGuiItemStackGroup itemStackGroup = recipeLayout.getItemStacks();
-        itemStackGroup.init(0, true, 22, 29); //Ore
-        itemStackGroup.init(1, true, 70, 19); //Crushed
-        itemStackGroup.init(2, true, 88, 19); //Crushed Purified
-        itemStackGroup.init(3, true, 106, 19); //Crushed Centrifuged
-        itemStackGroup.init(4, true, 70, 37); //Dust Impure
-        itemStackGroup.init(5, true, 88, 37); //Dust Purified
-        itemStackGroup.init(6, true, 106, 37); //Dust
+        // absolute mess, will rewrite once i start messing with OreByProduct
+        for (int i = 0; i < BASIC_PREFIX_LOCATIONS.size(); i+=2) {
+            itemStackGroup.init(i, true, BASIC_PREFIX_LOCATIONS.get(i), BASIC_PREFIX_LOCATIONS.get(i + 1));
+        }
 
-        for (int i = 0; i < recipeWrapper.getOutputCount(); i++)
-            itemStackGroup.init(i + 7, false, 70 + (i * 18), 59);
+        if (recipeWrapper.getOutputCount() > BYPRODUCT_LOCATIONS.size()/2) {
+            for (int i = 0; i < BYPRODUCT_LOCATIONS.size(); i+=2) {
+                itemStackGroup.init(i + BASIC_PREFIX_LOCATIONS.size(), false, BYPRODUCT_LOCATIONS.get(i), BYPRODUCT_LOCATIONS.get(i + 1));
+            }
+            // todo i guess? overflow past normal slots
+        }
+        else {
+            for (int i = 0; i < recipeWrapper.getOutputCount(); i++) {
+                itemStackGroup.init(i + BASIC_PREFIX_LOCATIONS.size(), false, BYPRODUCT_LOCATIONS.get(2 * i), BYPRODUCT_LOCATIONS.get((2 * i) + 1));
+            }
+        }
+
 
         itemStackGroup.addTooltipCallback(recipeWrapper::addTooltip);
         itemStackGroup.set(ingredients);
@@ -55,19 +81,13 @@ public class OreByProductCategory extends PrimitiveRecipeCategory<OreByProduct, 
 
     @Override
     public void drawExtras(Minecraft minecraft) {
-        this.slot.draw(minecraft, 22, 29);
-        this.arrowBackground.draw(minecraft, 44, 28);
-        this.arrowAnimation.draw(minecraft, 44, 28);
+        this.arrowsStatic.draw(minecraft, 47, 22);
 
-        this.slot.draw(minecraft, 70, 19);
-        this.slot.draw(minecraft, 88, 19);
-        this.slot.draw(minecraft, 106, 19);
-        this.slot.draw(minecraft, 70, 37);
-        this.slot.draw(minecraft, 88, 37);
-        this.slot.draw(minecraft, 106, 37);
-
-        for (int i = 0; i < 4; i++)
-            this.slot.draw(minecraft, 70 + (i * 18), 59);
+        for (int i = 0; i < BASIC_PREFIX_LOCATIONS.size(); i+=2) {
+            this.slot.draw(minecraft, BASIC_PREFIX_LOCATIONS.get(i), BASIC_PREFIX_LOCATIONS.get(i + 1));
+        }
+        for (int i = 0; i < BYPRODUCT_LOCATIONS.size(); i+=2) {
+            this.slot.draw(minecraft, BYPRODUCT_LOCATIONS.get(i), BYPRODUCT_LOCATIONS.get(i + 1));
+        }
     }
-
 }
